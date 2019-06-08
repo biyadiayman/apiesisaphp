cac4369cd9b153cdec50e067b6b016430b51a8d5
biyadiayman <35957421+biyadiayman@users.noreply.github.com>
2019-06-08 15:59:56 +0000

    Update EtudiantController.php

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -44,7 +44,7 @@ class EtudiantController extends Controller
             //$etudiant->save();
             return $etudiant;
         }else{
-            return 'Wrong info '.json_encode($etudiant->dateNaissance).'xoxo';
+            return 'Wrong info '.json_encode($etudiant->prenom == $request->prenom).'xoxo';
         }
         }else{
             return 'Wrong info (not set)';
